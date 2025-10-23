197733baca4e80bc974b18b4758ca77ba9a36e49
hetarho <sunlikeperson@gmail.com>
2025-10-23 18:01:21 +0900

    chore: ssr to ssg

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // SSG (Static Site Generation) mode enabled
-  ssr: true,
+  ssr: false,
   async prerender() {
     return ["/"];
   },
